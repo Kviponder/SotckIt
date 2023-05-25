0f73cd137c489103a99ccbc8ab9a14cd1699fc01
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-05-24 22:23:34 -0400

    Fixed error

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+        //Id column is the primary key because it is the unique identifier for each row of data
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true
+        },  
+        category_name: {    //category_name column is a String and can't be null
+          type: DataTypes.STRING,
+          allowNull: false
+        }
   },
   {
     sequelize,
