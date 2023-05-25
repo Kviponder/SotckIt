f1a59e9f35a6c39c6e62509f8e57c22e0b642026
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-05-25 13:25:17 -0400

    Added some routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,53 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get("/", (req, res) => {
+  Category.findAll({          //findAll is a Sequelize method to find all instances based on the model we're working with
+    include: [Product],       //Including the Product model in the Category model
+  })
+    .then((categories) => res.json(categories))
+    .catch((err) => res.status(500).json(err));
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", (req, res) => {
+  Category.findOne({
+    //findOne is a Sequelize method to find one instance based on the model we're working with
+    where: {
+      id: req.params.id, //Finding one category by its id
+    },
+    include: [Product], //Including the Product model in the Category model
+  })
+    .then((category) => res.json(category))
+    .catch((err) => res.status(500).json(err));
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post("/", (req, res) => {
+  Category.create(req.body) //create is a Sequelize method for creating a new instance based on the model we're working with
+    .then((category) => res.json(category))
+    .catch((err) => res.status(500).json(err));
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", (req, res) => {
+  Category.update(req.body, {     //update is a Sequelize method for updating an instance based on the model we're working with and the object we send in as an argument
+    where: {
+      id: req.params.id, 
+    },
+  })
+    .then((category) => res.json(category))
+    .catch((err) => res.status(500).json(err));
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete("/:id", (req, res) => {
+Categorey.destroy({     //destroy is a Sequelize method for deleting an instance based on the model we're working with and the object we send in as an argument
+    where: {
+      id: req.params.id,      
+    },
+  })
+    .then((category) => res.json(category))
+    .catch((err) => res.status(500).json(err));
 });
+
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,22 +1,45 @@
-const router = require('express').Router();
-const { Product, Category, Tag, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get("/", (req, res) => {
+  Product.findAll({
+    //This is a Sequelize method to find all instances based on the model we're working with
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((products) => res.json(products))
+    .catch((err) => res.status(500).json(err));
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get("/:id", (req, res) => {
+  Product.findOne({
+    //This is a Sequelize method to find one instance based on the model we're working with
+    where: {
+      id: req.params.id, //Finding one product by its id
+    },
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((product) => res.json(product))
+    .catch((err) => res.status(500).json(err));
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -48,7 +71,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -89,7 +112,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
 });
 
