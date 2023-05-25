a67f9baff534e9780d22216f22d9ec285098228f
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-05-24 22:31:26 -0400

    Added product and product tags models

--- models/Product.js
+++ models/Product.js
@@ -1,7 +1,7 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,14 +9,45 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2), //The price column will be a decimal and validates it below
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      defaultValue: 10,     //The stock column will be an integer with a default value of 10 and below is the validation
+      validate: {
+        isNumeric: true,
+      }
+  },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",   //The category model is imported at the top of the file from category.js
+        key: "id",
+      },
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,8 +6,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
-
+   id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+   }, 
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',   //The product model is imported at the top of the file from product.js
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',     //The Tag model is imported at the top of the file from tag.js
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
