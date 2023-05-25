db4fcf841f20df4a5abd50a16c272a2448749de3
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-05-24 22:22:25 -0400

    Added category model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,17 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    //Id column is the primary key because it is the unique identifier for each row of data
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },  
+    category_name: {    //category_name column is a String and can't be null
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
